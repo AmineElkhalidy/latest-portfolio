961fca39bb69c6c7056f66e32e996eb8e934481c
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-06-14 08:01:57 +0100

    4- About page is finished!

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,193 @@
-import React from "react";
+"use client";
+
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { about, education, experience } from "@/constants";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
+const skills = {
+  title: "My skills",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus labore corporis voluptas ab odit nesciunt officia sed!",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      title: "HTML 5",
+    },
+    {
+      icon: <FaCss3 />,
+      title: "CSS 3",
+    },
+    {
+      icon: <FaJs />,
+      title: "JavaScript",
+    },
+    {
+      icon: <FaReact />,
+      title: "React.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      title: "Next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      title: "TailwindCSS",
+    },
+    {
+      icon: <FaNodeJs />,
+      title: "Node.js",
+    },
+  ],
+};
 
 const About = () => {
-  return <div></div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px] "
+        >
+          <TabsList className="flex flex-col w-full max-w-[300px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          {/* Content */}
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left ">
+                <h3 className="text-3xl font-bold">{experience?.title}</h3>
+                <p className="max-w-[600px] text-muted-foreground mx-auto xl:mx-0">
+                  {experience?.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => (
+                      <li
+                        key={index}
+                        className="bg-gray-100 h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                      >
+                        <span className="text-sky-700">{item.duration}</span>
+                        <h3 className="text-lg max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-sky-700"></span>
+                          <p className="">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left ">
+                <h3 className="text-3xl font-bold">{education?.title}</h3>
+                <p className="max-w-[600px] text-muted-foreground mx-auto xl:mx-0">
+                  {education?.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => (
+                      <li
+                        key={index}
+                        className="bg-gray-100 h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                      >
+                        <span className="text-sky-700">{item.duration}</span>
+                        <h3 className="text-lg max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          {item.degree}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-sky-700"></span>
+                          <p className="">{item.institution}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="skills" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-3xl font-bold">{skills?.title}</h3>
+                <p className="max-w-[600px] text-muted-foreground mx-auto xl:mx-0">
+                  {skills?.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                    {skills.skillList.map((skill, index) => (
+                      <li key={index} className="">
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-gray-100 rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-sky-700 transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                              <TooltipContent>
+                                <p className="capitalize">{skill.title}</p>
+                              </TooltipContent>
+                            </TooltipTrigger>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px] ">
+                <h3 className="text-3xl font-semibold">{about.title}</h3>
+                <p className="max-w-[600px] text-muted-foreground mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-x-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => (
+                    <li
+                      key={index}
+                      className="flex items-center justify-center xl:justify-start gap-4"
+                    >
+                      <span className="text-muted-foreground">
+                        {item.fieldName}
+                      </span>
+                      <span className="text-lg">{item.fieldValue}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
 
 export default About;
--- constants/index.ts
+++ constants/index.ts
@@ -51,3 +51,102 @@ export const services = [
     href: "",
   },
 ];
+
+export const about = {
+  title: "About me",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus labore corporis voluptas ab odit nesciunt officia sed!",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Amine Elkhalidy",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+212) 611154307",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2+ Years",
+    },
+    {
+      fieldName: "Discord",
+      fieldValue: "Amine Elkhalidy",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Moroccan",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "a.amineelkhalidy@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "Arabic, French and English",
+    },
+  ],
+};
+
+export const experience = {
+  icon: "/assets/resume/badge.svg",
+  title: "My experience",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus labore corporis voluptas ab odit nesciunt officia sed!",
+  items: [
+    {
+      company: "Upwork",
+      position: "Front End Developer",
+      duration: "2022 - Present",
+    },
+    {
+      company: "LaunchEffect Agency",
+      position: "Front End Developer",
+      duration: "01/2023 - 06/2023",
+    },
+
+    {
+      company: "Fiverr",
+      position: "Front End Developer",
+      duration: "01/2024 - Present",
+    },
+  ],
+};
+
+export const education = {
+  icon: "/assets/resume/cap.svg",
+  title: "My education",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus labore corporis voluptas ab odit nesciunt officia sed!",
+  items: [
+    {
+      institution: "EST SAFI",
+      degree: "Associate degree in Software Engineering",
+      duration: "2018 - 2019",
+    },
+    {
+      institution: "EST SB",
+      degree: "Associate degree in Software Engineering",
+      duration: "2019 - 2021",
+    },
+    {
+      institution: "Udemy",
+      degree: "Software Engineering",
+      duration: "2021 - Present",
+    },
+    {
+      institution: "Freecodecamp",
+      degree: "Software Engineering",
+      duration: "2021 - Present",
+    },
+    {
+      institution: "Codedamn",
+      degree: "Software Engineering",
+      duration: "2021 - Present",
+    },
+  ],
+};
